1af8eddd9054e82347e497b49d23677624e0e46e
joseorteha <joseortegahac@gmail.com>
2025-05-25 09:39:32 -0600

    Tus cambios en Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -403,7 +403,7 @@ const Dashboard = () => {
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 0.3 }}
-          >
+                  >
             <CampaignFilters
               searchTerm={searchTerm}
               setSearchTerm={setSearchTerm}
@@ -434,7 +434,7 @@ const Dashboard = () => {
                 onDelete={handleDelete}
                 renderStatusBadge={renderStatusBadge}
               />
-            ))
+                  ))
           )}
         </div>
         {/* Información Adicional */}
